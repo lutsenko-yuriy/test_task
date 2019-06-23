8556748b23cd5f7ac972140c9a9cb62ce065d6b2
lutsenko-yuriy <lutsenko.yuriy93@gmail.com>
2019-06-23 17:08:53 +0300

    Layouts and dimens

--- app/src/main/java/com/yurich/test_task/ui/MainActivity.kt
+++ app/src/main/java/com/yurich/test_task/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.yurich.test_task.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.yurich.test_task.R
+import com.yurich.test_task.viewmodels.AlbumsViewState
 import com.yurich.test_task.viewmodels.MainActivityViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -13,5 +14,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel.albumsStateLiveData.observe(this) { viewState ->
+            processViewState(viewState)
+        }
+
+        if (savedInstanceState == null) {
+            viewModel.refreshAlbumsData()
+        } else {
+            viewModel.loadAlbumsData()
+        }
+    }
+
+    private fun processViewState(viewState: AlbumsViewState) {
+        when (viewState) {
+            is AlbumsViewState.ActualAlbums -> TODO()
+            is AlbumsViewState.NonActualAlbums -> TODO()
+            AlbumsViewState.Loading -> TODO()
+            AlbumsViewState.Error -> TODO()
+        }
     }
 }
