102593ec6c681134098d381863bdca20eb5a7c7a
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2019-09-22 01:01:53 +0300

    Some message

--- app/src/main/java/com/yurich/test_task/data/repository/AlbumsRepositoryImpl.kt
+++ app/src/main/java/com/yurich/test_task/data/repository/AlbumsRepositoryImpl.kt
@@ -2,37 +2,26 @@ package com.yurich.test_task.data.repository
 
 import com.yurich.test_task.data.database.AlbumsDaoDelegate
 import com.yurich.test_task.data.network.AlbumsServiceDelegate
-import kotlinx.coroutines.Dispatchers.Default
-import kotlinx.coroutines.withContext
 
 class AlbumsRepositoryImpl(
     private val serviceDelegate: AlbumsServiceDelegate,
     private val daoDelegate: AlbumsDaoDelegate
 ) : AlbumsRepository {
 
-    override suspend fun refreshAlbumsData(): AlbumsDataState = withContext(Default) {
-        val albumsFromNetwork = albumsFromNetwork()
-        if (albumsFromNetwork.isNotEmpty()) {
-            return@withContext AlbumsDataState.FromNetwork(albumsFromNetwork)
-        }
-
-        val albumsFromLocalStorage = albumsFromDatabase()
-        if (albumsFromLocalStorage.isNotEmpty()) {
-            return@withContext AlbumsDataState.FromDatabase(albumsFromLocalStorage)
-        }
-
-        return@withContext AlbumsDataState.FromNowhere
-    }
-
-    private suspend fun albumsFromNetwork(): List<Album> {
+    override suspend fun refreshAlbumsData(): AlbumsDataState {
         val albumsFromNetwork = serviceDelegate.getAlbums()
 
         if (albumsFromNetwork.isNotEmpty()) {
             daoDelegate.refreshAlbums(albumsFromNetwork)
+            return AlbumsDataState.FromNetwork(albumsFromNetwork)
         }
 
-        return albumsFromNetwork
+        val albumsFromLocalStorage = daoDelegate.getAlbums()
+        if (albumsFromLocalStorage.isNotEmpty()) {
+            return AlbumsDataState.FromDatabase(albumsFromLocalStorage)
+        }
+
+        return AlbumsDataState.FromNowhere
     }
 
-    private suspend fun albumsFromDatabase() = daoDelegate.getAlbums()
 }
